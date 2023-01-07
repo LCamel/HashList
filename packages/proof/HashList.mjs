8ccf531310bc45a633348030535cb4f9ab0d3070
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-01-07 19:16:31 +0800

    HashList.mjs: generateProof()

--- packages/proof/HashList.mjs
+++ packages/proof/HashList.mjs
@@ -1,14 +1,17 @@
 "use strict";
-import { poseidon } from "circomlibjs";
+import { poseidon } from "circomlibjs"; // for off-line computing
+import { groth16 } from "snarkjs";
 
 class HashList {
     constructor(hashInputCount) {
         this.hashInputCount = hashInputCount;
-        this.length = 0;
+        this.list = [];    // for generating circuit inputs
+                           // the on-chain HashList doesn't have this field
+        //this.length = 0; // since we have this.list, just use its length instead
         this.buf = new Array(hashInputCount);
     }
     add(item) {
-        const len = this.length;
+        const len = this.list.length;
         if (len <= 1) {
             this.buf[len] = BigInt(item);
         } else {
@@ -18,86 +21,57 @@ class HashList {
             }
             this.buf[index] = BigInt(item);
         }
-        this.length = len + 1;
-        console.log(">>>> buf");
-        for (var i = 0; i < this.hashInputCount; i++) {
-            console.log(this.buf[i]);
-        }
-        console.log("<<<< buf");
+        this.list.push(BigInt(item)); // TODO: dedup
     }
-    hash() {
+    _getZeroPaddedBuffer() {
         // 0 1 2 3 4 5 6 7 8 9 10
         // 0 1 2 3 4 2 3 4 2 3 4
-        const len = this.length;
+        const len = this.list.length;
         const bound = (len <= 1) ? len : (len - 2) % (this.hashInputCount - 1) + 2;
         const tmp = new Array(this.hashInputCount);
         for (let i = 0; i < this.hashInputCount; i++) {
             tmp[i] = (i < bound) ? this.buf[i] : BigInt(0);
         }
-        return poseidon(tmp);
+        return tmp;
+    }
+    generateCircuitInput(depth, arity) {
+        // TODO: consider hashCount!
+
+        const len = this.list.length;
+        const leafCount = arity ** depth;
+        if (len > leafCount) {
+            throw "length > arity ** depth";
+        }
+
+        var inputs = new Array(leafCount); // TODO: consider hashCount!
+        for (let i = 0; i < leafCount; i++) {
+            inputs[i] = (i < len) ? this.list[i] : BigInt(0);
+        }
+
+        const circuitInput = {
+            inputs: inputs,
+            length: len,
+            outputHashSelector: (len < 2) ? 0 : Math.floor((len - 2) / (this.hashInputCount - 1)),
+            buf: this._getZeroPaddedBuffer()
+        };
+
+        return circuitInput;
+    }
+    // returns a promise of the full proof
+    generateProof(depth, arity, wasm, zkey) {
+        const circuitInput = this.generateCircuitInput(depth, arity);
+        return groth16.fullProve(circuitInput, wasm, zkey);
     }
 }
 
-
-/*
-var h = new Array(HASH_COUNT);
-h[0] = poseidon([0, 1, 2, 3]);
-console.log(h[0]);
-h[1] = poseidon([h[0], 4, 5, 6]);
-console.log(h[1]);
-h[2] = poseidon([h[1], 7, 8, 9]);
-console.log(h[2]);
-h[3] = poseidon([h[2], 10, 11, 12]);
-console.log(h[3]);
-h[4] = poseidon([h[3], 13, 14, 15]);
-console.log(h[4]);
-h[5] = poseidon([h[4], 16, 17, 18]);
-console.log(h[5]);
-*/
-
-function eq(v1, v2) {
-    if (v1 != v2) throw "bad value: " + v1 + " " + v2;
-    console.log(v1);
-}
-
-console.log("=======");
 const hl = new HashList(4);
-eq(hl.hash(), poseidon([0, 0, 0, 0]));
+for (let i = 1; i <= 16; i++) {
+    hl.add(i);
+}
+//BigInt.prototype.toJSON = function() { return this.toString() }
+//console.log(JSON.stringify(hl.generateCircuitInput(4, 2)));
 
-console.log("adding 0 1 2 3");
-hl.add(0);
-eq(hl.hash(), poseidon([0, 0, 0, 0]));
-hl.add(1);
-eq(hl.hash(), poseidon([0, 1, 0, 0]));
-hl.add(2);
-eq(hl.hash(), poseidon([0, 1, 2, 0]));
-hl.add(3);
-eq(hl.hash(), poseidon([0, 1, 2, 3]));
-
-console.log("adding 4 5 6");
-hl.add(4);
-eq(hl.hash(), poseidon([poseidon([0, 1, 2, 3]), 4, 0, 0]));
-hl.add(5);
-eq(hl.hash(), poseidon([poseidon([0, 1, 2, 3]), 4, 5, 0]));
-hl.add(6);
-eq(hl.hash(), poseidon([poseidon([0, 1, 2, 3]), 4, 5, 6]));
-
-console.log("adding 7 8 9");
-hl.add(7);
-eq(hl.hash(), poseidon([poseidon([poseidon([0, 1, 2, 3]), 4, 5, 6]), 7, 0, 0]));
-hl.add(8);
-eq(hl.hash(), poseidon([poseidon([poseidon([0, 1, 2, 3]), 4, 5, 6]), 7, 8, 0]));
-hl.add(9);
-eq(hl.hash(), poseidon([poseidon([poseidon([0, 1, 2, 3]), 4, 5, 6]), 7, 8, 9]));
-console.log("adding 10");
-hl.add(10);
-eq(hl.hash(), poseidon([poseidon([poseidon([poseidon([0, 1, 2, 3]), 4, 5, 6]), 7, 8, 9]), 10, 0, 0]));
-
-console.log("=====");
-const hl2 = new HashList(4);
-hl2.add(4);
-hl2.add(5);
-hl2.add(6);
-hl2.add(7);
-hl2.add(8);
-eq(hl2.hash(), poseidon([poseidon([4, 5, 6, 7]), 8, 0, 0]));
+const wasm = "../circuits/out/HashList4Depth4Arity2_js/HashList4Depth4Arity2.wasm";
+const zkey = "../circuits/out/HashList4Depth4Arity2_js/HashList4Depth4Arity2_0001.zkey";
+const proof = await hl.generateProof(4, 2, wasm, zkey);
+console.log(JSON.stringify(proof));
