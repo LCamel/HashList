f36194acf8d079f77d07c8ec3cfb7c2fdda9b7fa
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-01-09 10:20:56 +0800

    add verifyMerkleRoot() in HashListExample.sol

--- packages/contracts/src/HashListExample.sol
+++ packages/contracts/src/HashListExample.sol
@@ -9,4 +9,7 @@ contract HashListExample4 {
     function add(uint item) public {
         hld.add(item);
     }
+    function verifyMerkleRoot(uint256[2] memory a, uint256[2][2] memory b, uint256[2] memory c, uint256 depth, uint256 arity, uint256 root) public view returns (bool) {
+        return hld.verifyMerkleRoot(a, b, c, depth, arity, root);
+    }
 }
