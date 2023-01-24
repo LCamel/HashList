2278da73a731c71d39fecbc783a7f31dd291a25b
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-01-24 22:21:39 +0800

    don't return lambda of Line.

--- doc/scene.py
+++ doc/scene.py
@@ -1,5 +1,3 @@
-
-
 from manim import *
 
 def b(x, y):
@@ -10,10 +8,12 @@ def b(x, y):
     return s
 
 def topToBottom(scene, b0, b1):
-    lam = lambda: Line((b0.get_vertices()[3] + b0.get_vertices()[2]) / 2, (b1.get_vertices()[0] + b1.get_vertices()[1]) / 2)
+    lam = lambda: Line((b0.get_vertices()[3] + b0.get_vertices()[2]) / 2,
+                       (b1.get_vertices()[0] + b1.get_vertices()[1]) / 2)
     line = lam()
     scene.add(line)
-    return line, lam
+    line.add_updater(lambda mobj: mobj.become(lam()))
+    return line
 
 class Boxes:
     def __init__(self, len, x0, y0):
@@ -25,8 +25,7 @@ class Boxes:
         for b1 in self.boxes:
             self.vGroup.add(b1)
 
-# !!!
-config.frame_width=20
+config.frame_width=20  # set frame_height doesn't work
 
 class A(MovingCameraScene):
 
@@ -38,25 +37,12 @@ class A(MovingCameraScene):
 
         bs2 = Boxes(4, 0, 0)
         self.add(bs2.vGroup)
-        #self.add(b0)
         b0 = bs2.boxes[0]
 
+        lines = [ topToBottom(self, bs.boxes[i], b0) for i in range(4) ]
 
-        lines = []
-        lineLams = []
-        for i in range(4):
-            line, lineLam = topToBottom(self, bs.boxes[i], b0)
-            lines.append(line)
-            line.add_updater(lambda mobj, lineLam=lineLam: mobj.become(lineLam()))
-            #lineLams.append(lineLam)
-
-        self.play(*[Create(lines[i]) for i in range(4)])
-        self.play(b0.animate.set_fill(BLUE, opacity=0.5))
-        #self.play(Create(b0))
-        #self.wait(1)
-
-        #for i in range(4):
-        #    lines[i].add_updater(lambda mobj, i=i: mobj.become(lineLams[i]()))
+        self.play(*[Create(line) for line in lines])
+        self.play(b0.animate.set_fill(BLUE, opacity=0.5))  # opacity is mandatary
 
         self.play(bs.vGroup.animate.shift(LEFT * 4))
         self.wait(1)
