a6595f92232756e693100ea1281d9211c3651726
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-01-24 20:26:37 +0800

    just create the box first. do not use the Create() animation

--- doc/r.sh
+++ doc/r.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #rm -fR media ; manim -s -ql --disable_caching scene.py A
-rm -fR media ; manim -ql --disable_caching scene.py A
+#rm -fR media ; manim -ql --disable_caching scene.py A
+rm -fR media ; manim -pql --disable_caching scene.py A
--- doc/scene.py
+++ doc/scene.py
@@ -2,8 +2,6 @@
 
 from manim import *
 
-def p(x, y):
-    return np.array([x, y, 0])
 def b(x, y):
     s = Polygon(np.array([x, y, 0]),
                 np.array([x+1, y, 0]),
@@ -28,31 +26,37 @@ class Boxes:
             self.vGroup.add(b1)
 
 # !!!
-config.frame_width=30
+config.frame_width=20
 
 class A(MovingCameraScene):
 
     def construct(self):
-        self.add(NumberPlane())
+        #self.add(NumberPlane())
 
         bs = Boxes(10, 0, -2)
         self.add(bs.vGroup)
 
-        b0 = b(2, 2)  # will be added later for Create()
+        bs2 = Boxes(4, 0, 0)
+        self.add(bs2.vGroup)
+        #self.add(b0)
+        b0 = bs2.boxes[0]
+
 
         lines = []
         lineLams = []
         for i in range(4):
             line, lineLam = topToBottom(self, bs.boxes[i], b0)
             lines.append(line)
-            lineLams.append(lineLam)
+            line.add_updater(lambda mobj, lineLam=lineLam: mobj.become(lineLam()))
+            #lineLams.append(lineLam)
 
         self.play(*[Create(lines[i]) for i in range(4)])
-        self.play(Create(b0))
+        self.play(b0.animate.set_fill(BLUE, opacity=0.5))
+        #self.play(Create(b0))
         #self.wait(1)
 
-        for i in range(4):
-            lines[i].add_updater(lambda mobj, i=i: mobj.become(lineLams[i]()))
+        #for i in range(4):
+        #    lines[i].add_updater(lambda mobj, i=i: mobj.become(lineLams[i]()))
 
         self.play(bs.vGroup.animate.shift(LEFT * 4))
         self.wait(1)
