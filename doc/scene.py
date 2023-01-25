e8628aa5bdf9e7ed2b656dd893ebd0a2de1399c0
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-01-25 18:36:25 +0800

    translate getLevelLengths()

--- doc/scene.py
+++ doc/scene.py
@@ -22,23 +22,43 @@ def connect(scene, o0, dir0, o1, dir1):
     line.add_updater(updater)
     return line
 
-config.frame_width=20  # set frame_height doesn't work
+def getLevelLengths(len, W, H):
+    lengths = [0] * H
+    zeroIfLessThan = 0  # W^0 + W^1 + W^2 ... (1 + 4 + 16 + ...)
+    for lv in range(H):
+        pow = W ** lv
+        zeroIfLessThan += pow
+        lvLen = 0 if len < zeroIfLessThan else (len - zeroIfLessThan) // pow % W + 1
+        lengths[lv] = lvLen
+        if lvLen == 0:
+            break
+    return lengths
+
+
+config.frame_width=30  # set frame_height doesn't work
 
 class A(MovingCameraScene):
 
     def construct(self):
         #self.add(NumberPlane())
 
+
         items = SquareList(10, 0, -2)
         self.add(items)
 
-        level = SquareList(4, 0, 0)
-        self.add(level)
+        W = 4
+        H = 5
 
-        lines = [ connect(self, items.square(i), UP, level.square(0), DOWN) for i in range(4) ]
+        lists = []
+        for lv in range(H):
+            list = SquareList(W, 0, lv * 2)
+            lists.append(list)
+            self.add(list)
+
+        lines = [ connect(self, items.square(i), UP, lists[0].square(0), DOWN) for i in range(4) ]
 
         self.play(*[Create(line) for line in lines])
-        self.play(level.square(0).animate.set_fill(BLUE, opacity=0.5))  # opacity is mandatary
+        self.play(lists[0].square(0).animate.set_fill(BLUE, opacity=0.5))  # opacity is mandatary
 
         self.play(items.animate.shift(LEFT * 4))
         self.wait(1)
