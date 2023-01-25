f765296aefe3804ddbad726067201c3e663185f9
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-01-25 10:51:24 +0800

    class SquareList(VGroup)

--- doc/scene.py
+++ doc/scene.py
@@ -1,28 +1,25 @@
 from manim import *
 
-def b(x, y):
+def squareBottomLeftAt(x, y):
     s = Square(1)
     s.set_x(x, LEFT)
     s.set_y(y, DOWN)
     return s
 
-def connect(scene, b0, dir0, b1, dir1):
-    lam = lambda: Line(b0.get_edge_center(dir0), b1.get_edge_center(dir1))
+class SquareList(VGroup):
+    def __init__(self, len, x0, y0):
+        super().__init__()
+        for i in range(len):
+            self.add(squareBottomLeftAt(x0 + i, y0))
+    def square(self, i):
+        return self.submobjects[i]
+
+def connect(scene, o0, dir0, o1, dir1):
+    lam = lambda: Line(o0.get_edge_center(dir0), o1.get_edge_center(dir1))
     line = lam()
     scene.add(line)
     line.add_updater(lambda mobj: mobj.become(lam()))
     return line
-
-class Boxes:
-    def __init__(self, len, x0, y0):
-        self.len = len
-        self.x0 = x0
-        self.y0 = y0
-        self.boxes = [b(x0 + i, y0) for i in range(len)]
-        self.vGroup = VGroup()
-        for b1 in self.boxes:
-            self.vGroup.add(b1)
-
 
 config.frame_width=20  # set frame_height doesn't work
 
@@ -31,18 +28,17 @@ class A(MovingCameraScene):
     def construct(self):
         #self.add(NumberPlane())
 
-        bs = Boxes(10, 0, -2)
-        self.add(bs.vGroup)
+        items = SquareList(10, 0, -2)
+        self.add(items)
 
-        bs2 = Boxes(4, 0, 0)
-        self.add(bs2.vGroup)
-        b0 = bs2.boxes[0]
+        level = SquareList(4, 0, 0)
+        self.add(level)
 
-        lines = [ connect(self, bs.boxes[i], UP, b0, DOWN) for i in range(4) ]
+        lines = [ connect(self, items.square(i), UP, level.square(0), DOWN) for i in range(4) ]
 
         self.play(*[Create(line) for line in lines])
-        self.play(b0.animate.set_fill(BLUE, opacity=0.5))  # opacity is mandatary
+        self.play(level.square(0).animate.set_fill(BLUE, opacity=0.5))  # opacity is mandatary
 
-        self.play(bs.vGroup.animate.shift(LEFT * 4))
+        self.play(items.animate.shift(LEFT * 4))
         self.wait(1)
 
