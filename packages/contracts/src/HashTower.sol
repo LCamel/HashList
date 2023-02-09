73805e14252e55c1f64d1b2b35085a55888db8dc
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-02-09 16:05:25 +0800

    inline length related functions to reduce 2000~3000 gas before: 38417 / 145051 / 231466 after : 35446 / 143041 / 229839

--- packages/contracts/src/HashTower.sol
+++ packages/contracts/src/HashTower.sol
@@ -18,47 +18,27 @@ contract HashTower {
 
     event Add(uint indexed level, uint indexed lvFullIndex, uint value); // TODO: merge
 
-    // TODO: inline this function
-    function getLevelFullLengths(uint64 len) private pure returns (uint64[H] memory) {
-        uint64[H] memory lengths;
+    function add(uint256 toAdd) public {
+        uint256 len = length; // the length before adding the item
         uint64 zeroIfLessThan = 0;
         uint64 pow = 1; // pow = W^lv
         for (uint8 lv = 0; lv < H; lv++) {
             zeroIfLessThan += pow; // W^0 + W^1 + W^2 ... (1 + 4 + 16 + ...)
-            uint64 lvLen = (len < zeroIfLessThan) ? 0 : (len - zeroIfLessThan) / pow + 1;
-            if (lvLen == 0) break;
-            lengths[lv] = lvLen;
-            pow *= W; // use shift if W is power of 2
-        }
-        return lengths;
-    }
-    function toInTowerLength(uint64 fullLen) private pure returns (uint64) {
-        return fullLen == 0 ? 0 : (fullLen - 1) % W + 1;
-    }
-    function getLevelInTowerLengths(uint64 len) private pure returns (uint64[H] memory) {
-        uint64[H] memory inTowerLengths;
-        uint64[H] memory fullLengths = getLevelFullLengths(len);
-        for (uint8 lv = 0; lv < H; lv++) {
-            inTowerLengths[lv] = toInTowerLength(fullLengths[lv]);
-        }
-        return inTowerLengths;
-    }
-    function add(uint256 item) public {
-        uint256 len = length; // the length before adding the item
-        uint64[H] memory lvFullLengths = getLevelFullLengths(uint64(len)); // TODO: inline this function in the loop (solidity)
-        uint256 toAdd = item;
-        for (uint8 lv = 0; lv < H; lv++) {
-            uint64 lvLen = toInTowerLength(lvFullLengths[lv]);
+            uint64 lvFullLen = uint64((len < zeroIfLessThan) ? 0 : (len - zeroIfLessThan) / pow + 1);
+            uint64 lvLen = lvFullLen == 0 ? 0 : (lvFullLen - 1) % W + 1;
+
             if (lvLen < W) { // not full
                 levels[lv][lvLen] = toAdd;
-                emit Add(lv, lvFullLengths[lv], toAdd);
+                emit Add(lv, lvFullLen, toAdd);
                 break;
             } else { // full
                 uint256 lvHash = P.poseidon(levels[lv]);
                 levels[lv][0] = toAdd; // add it in the now-considered-being-emptied level
-                emit Add(lv, lvFullLengths[lv], toAdd);
+                emit Add(lv, lvFullLen, toAdd);
                 toAdd = lvHash; // to be added in the upper level
             }
+
+            pow *= W; // use shift if W is power of 2
         }
         length = len + 1;
     }
