8ca648e44cde89abb1ea8def7766b0dd2a136972
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-02-26 23:01:22 +0800

    show formatted L and S

--- packages/proof/Correctness.mjs
+++ packages/proof/Correctness.mjs
@@ -1,34 +1,36 @@
 "use strict";
-function Basic(W, digest) {
-    let L = [];
+function Tower(W, digest) {
+    let S = []; // shifted levels
+    let L = []; // levels
     function _add(lv, v) {
-        if (lv == L.length) {
+        if (lv == L.length) {          // new level
+            S[lv] = [];
             L[lv] = [v];
-        } else if (L[lv].length < W) {
+        } else if (L[lv].length < W) { // not full
             L[lv].push(v);
-        } else {
+        } else {                       // full
             const d = digest(L[lv]);
+            S[lv].push(...L[lv]);      // shift
             L[lv] = [v];
-            _add(lv + 1, d);
+            _add(lv + 1, d);           // up
         }
     }
     let add = (item) => _add(0, item);
-    let getShape = () => L.map(l => l.length);
-    return { W, digest, L, add, getShape };
+    return { W, digest, L, S, add };
 }
-/*
-function digest(vs) {
-    var tmp = vs.flat();
-    return [tmp[0], tmp[tmp.length - 1]];
-}
-*/
 
-function getShape(len) {
-    var t = Basic(4, _ => 1);
-    for (let i = 0; i < len; i++) {
-        t.add(i);
+function digestOfRange(vs) {
+    var arr = vs.flat();
+    return [ arr.at(0), arr.at(-1) ];
+}
+
+var t = Tower(4, digestOfRange);
+for (let i = 0; i < 150; i++) {
+    t.add(i);
+
+    console.log("\n");
+    for (var lv = t.L.length - 1; lv >= 0; lv--) {
+        console.log(t.S[lv].join(" ").padStart(70).slice(-70), "#",
+            t.L[lv].map(v => v.toString().padStart(7)).join(" "));
     }
-    return t.getShape();
 }
-
-console.log(getShape(25));
