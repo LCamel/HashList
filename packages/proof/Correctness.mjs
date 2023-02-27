18a8950f663159f587999fb23ccf0b9a1218634d
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-02-27 19:15:20 +0800

    add getShape for Correctness.mjs

--- packages/proof/Correctness.mjs
+++ packages/proof/Correctness.mjs
@@ -24,8 +24,9 @@ function digestOfRange(vs) {
     return [ arr.at(0), arr.at(-1) ];
 }
 
+const N = 150;
 var t = Tower(4, digestOfRange);
-for (let i = 0; i < 150; i++) {
+for (let i = 0; i < N; i++) {
     t.add(i);
 
     console.log("\n");
@@ -34,3 +35,44 @@ for (let i = 0; i < 150; i++) {
             t.L[lv].map(v => v.toString().padStart(7)).join(" "));
     }
 }
+
+const assert = (v, msg) => { if (!v) throw msg };
+//t.getCount = () => t.L.reduce((acc, l, i) => acc + l.length * t.W ** i, 0);
+//const count = t.getCount();
+//console.log("count: ", count);
+//assert(count == N, "bad count");
+
+
+// given a single number "count", can we reconstruct the shape of L ?
+function getShape(count) {
+    const FL = []; // full length
+    const LL = []; // level length
+    const START = []; // full level index of the first elements of each level
+    for (let lv = 0, z = 0; true; lv++) {
+        z += t.W ** lv;
+        if (count < z) break;
+        let fl = Math.floor((count - z) / t.W ** lv) + 1;
+        let ll = (fl - 1) % t.W + 1;
+        let start = fl - ll;
+        FL.push(fl);
+        LL.push(ll);
+        START.push(start);
+    }
+    return [FL, LL, START];
+}
+
+t = Tower(4, digestOfRange);
+for (let i = 0; i < N; i++) {
+    t.add(i);
+
+    let [FL, LL, START] = getShape(i + 1);
+    for (let lv = 0; lv < t.L.length; lv++) {
+        assert(FL[lv] == t.S[lv].length + t.L[lv].length, "bad FL");
+        assert(LL[lv] == t.L[lv].length, "bad LL");
+        assert(START[lv] * t.W ** lv == t.L[lv].flat()[0], "bad START");
+    }
+}
+
+//console.log("FL: ", FL);
+//console.log("LL: ", LL);
+//console.log("START: ", START);
