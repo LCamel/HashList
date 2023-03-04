ffaa65581599fecb6982a0803254ad8b3ab3bfae
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-03-04 13:46:55 +0800

    add anvil.sh stop

--- packages/POC/scripts/anvil.sh
+++ packages/POC/scripts/anvil.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cd $SCRIPT_DIR
+
+if [ "$1" = "stop" ]; then
+    kill -9 `cat /tmp/anvil/anvil.pid`
+    echo STOP
+    exit 0;
+fi
 
 cast chain-id >/dev/null 2>&1 && exit  # if already running
+
+echo "chdir: /tmp/anvil"
+mkdir -p /tmp/anvil
+cd /tmp/anvil
+
 
 # https://github.com/foundry-rs/foundry/issues/3573
 # error: { code: -32603, message: 'EVM error InvalidOpcode' }
@@ -11,7 +21,7 @@ cast chain-id >/dev/null 2>&1 && exit  # if already running
 #source .env
 #anvil --gas-limit 30000000 --fork-url=${FORK_URL} --fork-block-number=8166444 -a 2 --steps-tracing > anvil.log 2>&1 &
 
-anvil --gas-limit 30000000 --load-state ./anvil_state_poseidon.json --steps-tracing > anvil.log 2>&1 &
+anvil --gas-limit 30000000 --load-state "${SCRIPT_DIR}/anvil_state_poseidon.json" --steps-tracing > anvil.log 2>&1 &
 
 
 PID=$!
