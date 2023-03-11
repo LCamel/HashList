c6930fa4facd034f025e965b67f6c1c54381b29a
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-03-12 00:07:47 +0800

    add circom_tester

---
+++ POC/test/CircuitTest.mjs
@@ -0,0 +1,36 @@
+// SEE: https://github.com/iden3/circuits/blob/a6aa4641f9b8736fab3e721be727701890d2a85e/test/comparators.test.ts
+import chai, { expect } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { fileURLToPath } from 'url';
+import * as path from "path";
+import { wasm as tester } from "circom_tester";
+//const F1Field = require("ffjavascript").F1Field;
+//const Scalar = require("ffjavascript").Scalar;
+//exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+//const Fr = new F1Field(exports.p);
+
+chai.use(chaiAsPromised);
+
+
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+describe("PickOne", function () {
+    this.timeout(200000);
+
+    it("PickOne", async () => {
+        const circuit = await tester(
+            path.join(__dirname, "circuits", "PickOne.circom"),
+            { reduceConstraints: false }
+        );
+
+        var witness = await circuit.calculateWitness({
+            in: [100, 200, 300, 400], sel: 2
+        });
+        await circuit.checkConstraints(witness);
+        await circuit.assertOut(witness, {out: 300});
+
+        await expect(circuit.calculateWitness({
+            in: [100, 200, 300, 400], sel: 4
+        })).to.be.rejected;
+    });
+});
