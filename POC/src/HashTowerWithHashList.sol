af27ebd7b144efea4780bd2635c5044e9ce99ece
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-03-22 17:08:01 +0800

    add check for toAdd and capacity

--- POC/src/HashTowerWithHashList.sol
+++ POC/src/HashTowerWithHashList.sol
@@ -2,15 +2,19 @@
 pragma solidity ^0.8.17;
 
 //import "./HashTowerWithHashListH20W4Verifier.sol";
-
 contract Poseidon2 {
     function poseidon(uint256[2] memory) public pure returns (uint256) {}
 }
 Poseidon2 constant P2 = Poseidon2(0x1111111122222222333333334444444400000002);
 
+// capacity = W * (W**0 + W**1 + ... + W**(H - 1)) = W * (W**H - 1) / (W - 1)
+// 4 * (4^0 + 4^1 + 4^2 + ... 4^19) = 1_466_015_503_700
 uint8 constant H = 20;
 uint8 constant W = 4;
+uint256 constant CAPACITY = 1_466_015_503_700; // https://github.com/ethereum/solidity/issues/13724
 
+uint256 constant SNARK_SCALAR_FIELD =
+    21888242871839275222246405745257275088548364400416034343698204186575808495617;
 
 
 contract HashTowerWithHashList {
@@ -22,24 +26,25 @@ contract HashTowerWithHashList {
     event Add(uint8 indexed level, uint64 indexed lvFullIndex, uint256 value); // TODO: merge fields
 
     function add(uint256 toAdd) public {
-        // TODO: check capacity or forcing a high capacity tower
-        // TODO: check toAdd value range?
+        require(toAdd < SNARK_SCALAR_FIELD, "HashTowerWithHashList: toAdd must be < SNARK_SCALAR_FIELD");
 
         uint64 count = _count;
+        require(count < CAPACITY, "HashTowerWithHashList: full");
+
         uint64 z;
         uint64 fl;
-        uint8 ll; // TODO: uint8
+        uint8 ll; // TODO: or just uint256 ?
         uint8 lv;
         //uint64 powWLv = 1; // powWLv = W ** lv // TODO
+
+        // find the lowest level that has space
         for (; true; lv++) {
-            // inlined length computations
             z += W ** lv;
             fl = (count < z) ? 0 : (count - z) / W ** lv + 1;
             ll = uint8((fl == 0) ? 0 : (fl - 1) % W + 1);
             if (ll < W) break;
         }
-
-        // now we are at the first level that has space
+        // append and go downward
         uint256 v = (lv > 0) ? D[lv - 1] : toAdd;
         emit Add(lv, fl, v);
         uint256 d = (ll == 0) ? v : P2.poseidon([D[lv], v]);
