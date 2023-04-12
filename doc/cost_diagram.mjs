bd82c30a64d57a25e675d7abc541157c7b117285
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-04-12 10:27:25 +0800

    change cost diagram to a gray scale one

--- doc/cost_diagram.mjs
+++ doc/cost_diagram.mjs
@@ -15,18 +15,19 @@ function add(lv, v) {
 }
 function digest(vs) { return 1 }
 
-var N = 30;
+var N = 150;
 var W = 4;
 var H = 4;
-var out = Array.from({length: H}, () => Array.from({length: N}, () => 1))
+var out = Array.from({length: H}, () => Array.from({length: N}, () => 255))
 for (var i = 0; i < N; i++) {
     for (var j = add(0, i); j >= 0; j--) {
-        out[j][i] = 0;
+        out[j][i] = 255 - 40 - j * 40;
     }
 }
 
-console.log("P1")
+console.log("P2")
 console.log(N, H)
+console.log("255")
 out.reverse().forEach((x) => console.log(x.join(' ')))
 
-// npx ts-node a.ts | convert pbm:- -scale 800% -bordercolor black -border 20 /tmp/b.png
+// node cost_diagram.mjs| convert pgm:- -scale 800% -bordercolor white -border 20 /tmp/b.png
